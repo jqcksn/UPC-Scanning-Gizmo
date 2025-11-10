83d6be14dba528e1b08debb801451c0a148b96e0
piberfaper <124284645+piberfaper@users.noreply.github.com>
2025-11-10 17:58:32 -0600

    Update gizmo.py

--- gizmo.py
+++ gizmo.py
@@ -1,26 +1,32 @@
 try:
     import os
+    import random
     filepath = os.path.dirname(os.path.abspath(__file__))
     os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
     import pygame
     for dirfile in os.listdir(filepath):
         if '.txt' in dirfile:
-            jerry = open(f"{filepath}{dirfile}", 'r')
-			print(f"Using: {dirfile}")
+            jerry = open(f"{filepath}\\{dirfile}", 'r')
+            print(f"Using: {dirfile}")
             break
     try:
         jerry
     except:
         print("There are no txts in your directory!")
     pygame.mixer.init()
-    lines = jerry.read()
+    lines = jerry.readlines()
     jerry.close()
     while True:
-        upc = f'{input("Scan the item! ")}\n'
-        if upc in lines:
-	        pygame.mixer.music.load(f'{filepath}\\sounds\\yes.mp3')
-        else:
-	        pygame.mixer.music.load(f'{filepath}\\sounds\\no.mp3')
+        upc = f'{input("Scan the item! ")}'
+        foundit = False
+        for line in lines:
+            if upc.lower() in line.lower():
+                pygame.mixer.music.load(f'{filepath}\\sounds\\yes.mp3')
+                print(f"Name: {line.split('\t')[0]}\nCondition: {line.split('\t')[7]}")
+                foundit = True
+        if not foundit:
+            pygame.mixer.music.load(f'{filepath}\\sounds\\no.mp3')
+            print(f"\033[31mDENIED. ({upc}) thing fucking SUCKS!!!!\033[0m")
         pygame.mixer.music.play()
 except Exception as e:
     print(e)
