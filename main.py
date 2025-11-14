709a0c52f1875ee46b04778f0193609fd516fb7b
jqcksn <65518991+jqcksn@users.noreply.github.com>
2025-11-14 15:47:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -26,15 +26,16 @@ pygame.mixer.init()
 running = True
 
 while running:
-    command = f'{input("Scan the item! ")}'
+    command = f'{input("Let me know what you want to let me know: ")}'
     columns = lines[0].strip().split('\t')
     columns = {k: v for k, v in enumerate(columns)}
-    match command:
+    match command.split(' ')[0]:
         case 'help':
             print(
                 """If you want to search the file for any of the commands, start the query with "iHateCommands"
 
 printcols: prints column names and numbers for settings.
+    -c: Clear right before, use if lines are truncated
 
 clear: clears console log
 
@@ -49,6 +50,8 @@ settings: accesses settings
         -r: Until "exit" typed
             (sorry lmao)""")
         case 'printcols':
+            if '-c' in command:
+                os.system('cls')
             print(*map(lambda t: f"{t[0]}: {t[1]}", enumerate(columns.values())), sep="\n")
         case 'clear':
             os.system('cls')
@@ -67,7 +70,7 @@ settings: accesses settings
                 if command.lower() in line.lower():
                     printed = ""
                     for num in range(len(columns)):
-                        if num in settings['columns']:
+                        if num in settings['Columns']:
                             printed += f"{columns[num]}: {line.split('\t')[num]}\n"
                     print(printed.rstrip())
                     found = True
@@ -77,7 +80,7 @@ settings: accesses settings
                 print(f"\033[31m({command}) not found in file\033[0m")
             else:
                     if settings['Muted']:
-                        if settings['text2speech']:
+                        if settings['Text to speech']:
                             readFile = gTTS(printed)
                             readFile.save('sounds\\temp.mp3')
                             pygame.mixer.music.load(f'{filepath}\\files\\sounds\\temp.mp3')
