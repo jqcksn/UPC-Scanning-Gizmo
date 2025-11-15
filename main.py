6b2d9d4b8f31dec53792b326d465605e0bba16ea
jacksonlivingston <jacksonlivingston6@gmail.com>
2025-11-15 14:46:09 -0600

    Refactor settings and sound logic; enhance settings UI
    Updated settings.json structure to add 'Sound' and 'Max speaking time' options. Refactored main.py to replace 'Muted' with 'Sound', improved settings menu with interactive adjustments, and fixed column selection logic. Sound and text-to-speech handling now respect new settings and allow for maximum speaking time. Added temp.mp3 to .gitignore.

--- main.py
+++ main.py
@@ -3,29 +3,20 @@ import os
 with open('files\\settings.json', 'r') as settinginit:
     settings = json.load(settinginit)
 print(settings)
-if not settings['Muted']:
-    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-    import pygame
-    import time
-    if settings['Text to speech']:
-        from gtts import gTTS
 filepath = os.path.dirname(os.path.abspath(__file__))
 texts = []
 for dirfile in os.listdir(filepath):
     if '.txt' in dirfile:
         texts.append(dirfile)
 if len(texts) > 1:
-    print(*map(lambda t: f"{t[0]}: {t[1]}", enumerate(texts)), sep="\n")
-    textfile = open(texts[int(input("Which number text file are you using?: "))], 'r')
+    print(*map(lambda t: f"{t[0 + 1]}: {t[1]}", enumerate(texts)), sep="\n")
+    textfile = open(texts[int(input("Which number text file are you using?: "))-1], 'r')
 else:
     textfile = open(texts[0], 'r')
 print(f"Using: {textfile.name}")
 lines = textfile.readlines()
 textfile.close()
-
-pygame.mixer.init()
 running = True
-
 while running:
     allem = False
     command = f'{input("Let me know what you want to let me know: ")}'
@@ -36,7 +27,7 @@ while running:
     match command.split(' ')[0]:
         case 'help':
             print(
-                """If you want to search the file for any of the commands, start the query with "^^^"
+"""If you want to search the file for any of the commands, start the query with "^^^"
 
 printcols: Prints column names and numbers for settings. Can also be typed after query to print all columns of query
 
@@ -45,22 +36,62 @@ printcols: Prints column names and numbers for settings. Can also be typed after
 exit: idk what this one does i'm scared
 
 settings: accesses settings
-    Text to speech: Toggles text to speech
-    Mute: Toggles sounds
-    Columns: Selection of what columns should be printed when an item is found""")
+    Sound: if sound should be enabled
+    Text to speech: If text to speech should be enabled
+    Columns: Selection of what columns should be printed when an item is found
+        
+        action examples: 
+        remove 10 20
+
+        remove: removes the following columns
+        rewrite: deletes all columns except for ones input
+        add: adds numbers to printed columns""")
         case 'printcols':
             print(*map(lambda t: f"{t[0]}: {t[1]}", enumerate(columns.values())), sep="\n")
         case 'clear':
             os.system('cls')
         case 'settings':
+            adjusting = True
+            while adjusting:
+                for index, [setting, values] in enumerate(settings.items()):
+                    if type(values) == type(True):
+                        print(f"{index + 1}: {setting} is {'on' if values else 'off'}")
+                    elif type(values) == type([]):
+                        print(f"{index + 1}: Columns printed: ", end="")
+                        for column in values:
+                            print(column, end=' ')
+                        print()
+                    else:
+                        print(f"{index + 1}: {setting} is {values}")
+                print("save: Save settings and exit")
+                settingCommand = input("Enter the index of your change: ")
+                match settingCommand:
+                    case '1':
+                        settings['Text to speech'] = not settings['Text to speech']
+                        print(f"Text to speech is now {'on' if settings['Text to speech'] else 'off'}")
+                    case '2':
+                        settings['Sound'] = not settings['Sound']
+                        print(f"Sound is now {'on' if settings['Sound'] else 'off'}")
+                    case '3':
+                        print(*map(lambda t: f"{t[0]}: {t[1]}", enumerate(columns.values())), sep="\n")
+                        settingCommand = input("Type your action (add/remove/rewrite) and column number(s): ")
+                        settingCommand = settingCommand.split()
+                        settingCommand = [int(col) if ind > 0 else col for ind, col in enumerate(settingCommand)]
+                        match settingCommand[0]:
+                            case 'add':
+                                for column in settingCommand[1:]:
+                                    settings['Columns'].append(column)
+                            case 'remove':
+                                settings['Columns'] = [column for column in settings['Columns'] if column not in settingCommand[1:]]
+                            case 'rewrite':
+                                settings['Columns'] = settingCommand[1:]
+                        settings['Columns'] = list(set(settings['Columns']))
+                    case '4':
+                        settings['Max speaking time'] = str(float(input("What is your desired maximum speaking time? (seconds): ")))
+                    case 'save':
+                        adjusting = False
             with open('files\\settings.json', 'w') as settingfile:
-                adjusting = True
-                while adjusting:
-                    print(settings)
-                    setcond = input("What would you like to change?: ")
-
-                        
-                json.dump(settings, settingfile)
+                    json.dump(settings, settingfile, indent=4)
         case 'exit':
             running = False
         case _:
@@ -74,22 +105,33 @@ settings: accesses settings
                     for num in range(len(columns)):
                         if num in settings['Columns'] or allem:
                             printed += f"{columns[num]}: {line.split('\t')[num]}\n"
-                    allem = False
                     print(printed.rstrip())
                     found = True
+            allem = False
             if not found:
-                if settings['Muted']:
+                if settings['Sound']:
+                    import pygame
                     pygame.mixer.music.load(f'{filepath}\\files\\sounds\\no.mp3')
                 print(f"\033[31m({command}) not found in file\033[0m")
             else:
-                    if settings['Muted']:
-                        if settings['Text to speech']:
-                            readFile = gTTS(printed)
-                            readFile.save('sounds\\temp.mp3')
-                            pygame.mixer.music.load(f'{filepath}\\files\\sounds\\temp.mp3')
-                            pygame.mixer.music.play()
-                            while pygame.mixer.music.get_busy():
-                                time.sleep(0.1)
-                        pygame.mixer.music.load(f'{filepath}\\files\\sounds\\yes.mp3')
-            if settings['Muted']:
+                if settings['Sound']:
+                    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+                    import time
+                    import pygame
+                    pygame.mixer.init()
+                    if settings['Text to speech']:
+                        from gtts import gTTS
+                        readFile = gTTS(printed)
+                        readFile.save('files\\sounds\\temp.mp3')
+                        pygame.mixer.music.load(f'{filepath}\\files\\sounds\\temp.mp3')
+                        pygame.mixer.music.play()
+                        start = time.time()
+                        while True:
+                            if not pygame.mixer.music.get_busy():
+                                break
+                            if time.time() - start >= float(settings['Max speaking time']):
+                                break
+                            time.sleep(0.05)
+                    pygame.mixer.music.load(f'{filepath}\\files\\sounds\\yes.mp3')
+            if settings['Sound']:
                 pygame.mixer.music.play()
